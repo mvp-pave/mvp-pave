8cc9ead92b08544a63158cbcaa115336ccdb1cc4
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-10-01 15:57:29 -0700

    added html

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import PromoHeader from './PromoHeader.js';
+// import SiteHeader from './SiteHeader.js';
+// import MainNavigation from './MainNavigation.js';
+// import '../styles/app.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+          {/* <PromoHeader /> */}
+          <div id="full-topbar">
+            <div id="content">
+
+              {/* <SiteHeader /> */}
+              {/* <MainNavigation /> */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
