cb135a993f63c730140d36a48c6ec8c1be2079dc
AK <ak@AKs-MacBook-Pro.local>
2019-10-01 13:49:19 -0700

    Update server and router files

--- server/index.js
+++ server/index.js
@@ -7,23 +7,26 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/../client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+app.use('/api', router);
+// app.get('/items', function (req, res) {
+//   items.selectAll(function(err, data) {
+//     if(err) {
+//       res.sendStatus(500);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
-});
+})
 
