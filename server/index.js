96654993c464ac0b414e2677fba435e19a91cc34
AK <ak@AKs-MacBook-Pro.local>
2019-10-01 17:05:38 -0700

    Finish connecting database helpers with router and server index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
-app.use('/', router)
+app.use('/pave', router)
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
