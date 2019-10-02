c85efc7b5c54875475c0f66b6a82d5517ebcc6c8
pauldev1128 <pauldev118@gmail.com>
2019-10-02 16:20:02 -0700

    able to get info from yelp using the yelp api

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,70 +1,70 @@
-import React, { Component } from 'react';
-import Topbar from './TopBar.js';
-import Search from './Search.js';
-import List from './List.js';
-import SuggestedBottom from './SuggestedBottom.js';
-import { IoMdSearch } from 'react-icons/fa';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import React, { Component } from 'react';
+// import Topbar from './TopBar.js';
+// import Search from './Search.js';
+// import List from './List.js';
+// import SuggestedBottom from './SuggestedBottom.js';
+// import { IoMdSearch } from 'react-icons/fa';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchClicked: false,
-    }
-    this.setWrapperRef = this.setWrapperRef.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-    this.handleClickInside = this.handleClickInside.bind(this);
-  }
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchClicked: false,
+//     }
+//     this.setWrapperRef = this.setWrapperRef.bind(this);
+//     this.handleClickOutside = this.handleClickOutside.bind(this);
+//     this.handleClickInside = this.handleClickInside.bind(this);
+//   }
 
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
-  }
+//   componentDidMount() {
+//     document.addEventListener('mousedown', this.handleClickOutside);
+//   }
 
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-  }
+//   componentWillUnmount() {
+//     document.removeEventListener('mousedown', this.handleClickOutside);
+//   }
 
-  setWrapperRef(node) {
-    this.wrapperRef = node;
-  }
+//   setWrapperRef(node) {
+//     this.wrapperRef = node;
+//   }
 
-  handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      this.setState({ searchClicked: false }, () => this.setState({ query: '', location: '' }));
-    }
-  }
+//   handleClickOutside(event) {
+//     if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+//       this.setState({ searchClicked: false }, () => this.setState({ query: '', location: '' }));
+//     }
+//   }
 
-  handleClickInside() {
-    this.setState({ searchClicked: true });
-  }
+//   handleClickInside() {
+//     this.setState({ searchClicked: true });
+//   }
 
-  // mouseOut() {
-  //   this.setState({ moused: false });
-  // }
+//   // mouseOut() {
+//   //   this.setState({ moused: false });
+//   // }
 
-  // mouseOver() {
-  //   this.setState({ moused: true });
-  // }
+//   // mouseOver() {
+//   //   this.setState({ moused: true });
+//   // }
 
-  render() {
-    return (
-      <Router>
-      <div>
-        <div id="full-topbar">
-          <h2>PAVÉ</h2>
-          <Link to="/search" onClick={this.handleClickInside} ><IoMdSearch /></Link>
-          <Search searchClicked={this.state.searchClicked} />
+//   render() {
+//     return (
+//       <Router>
+//       <div>
+//         <div id="full-topbar">
+//           <h2>PAVÉ</h2>
+//           <Link to="/search" onClick={this.handleClickInside} ><IoMdSearch /></Link>
+//           <Search searchClicked={this.state.searchClicked} />
 
-          <Topbar />
-          <div id="content">
-            <List />
-            <SuggestedBottom />
-          </div>
-        </div>
-        <Switch>
-        <Route exact path="/search" component={Search} />
-      </Router>
-    )
-  }
-}
+//           <Topbar />
+//           <div id="content">
+//             <List />
+//             <SuggestedBottom />
+//           </div>
+//         </div>
+//         <Switch>
+//         <Route exact path="/search" component={Search} />
+//       </Router>
+//     )
+//   }
+// }
