abba6b5fa4ac77ea58ee9a917aa01746c575d150
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2019-10-01 18:46:59 -0700

    Edited App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import Login from './Login.js';
-import Home from './Logout.js';
-import fire from '../config/Fire.js';
-// import PromoHeader from './PromoHeader.js';
-// import SiteHeader from './SiteHeader.js';
-// import MainNavigation from './MainNavigation.js';
-// import '../styles/app.css';
+import Topbar from './TopBar.js';
+import Search from './Search.js';
+import List from './List.js';
+import SuggestedBottom from './SuggestedBottom.js';
 
 export default class App extends Component {
   constructor(props) {
@@ -16,33 +13,18 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    this.authListener();
+    
   }
 
-  authListener() {
-    fire.auth().onAuthStateChanged(function (user) {
-      console.log('user:', user);
-      if (user) {
-        // User is signed in.
-        this.setState({ user }, () => console.log('user –––––> ', this.state.user));
-      } else {
-        // No user is signed in.
-        this.setState({ user: null }, () => console.log('user –––––> ', this.state.user))
-      }
-    });
-  }
 
   render() {
     return (
       <div>
-        {this.state.user ? (<Home />) : (<Login />)}
-        {/* <PromoHeader /> */}
-        {/* <div id="full-topbar"> */}
-        {/* <div id="content"> */}
-        {/* <SiteHeader /> */}
-        {/* <MainNavigation /> */}
-        {/* </div> */}
-        {/* </div> */}
+        <h2>PAVÉ</h2>
+        <Search />
+        <Topbar />
+        <List />
+        <SuggestedBottom />
       </div>
     )
   }
