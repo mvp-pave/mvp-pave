f2f651ac22c5d579ec5b02c49b2d31439db24c19
Arthur Kim <40188929+ArthurKimGl19@users.noreply.github.com>
2019-10-03 09:53:40 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-let REACT_APP_YELP_API_KEY = function() {
-  return 'Bearer caXRZOBNxfvn601tR3oHPXwiJajsWUcOHX5nV85P0Va0kf26m8_KJzyJ4V4IPgDiPXhRLSPwSS3aH4spZ8Rhbzaefw09Lne5LjFHquwVoJk7at9EhfajAiKB8VKVXXYx'
- }
- module.exports = REACT_APP_YELP_API_KEY;
