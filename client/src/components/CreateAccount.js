1d4e7334435cfff3abc4a94b194a24a468c5dd81
AK <ak@AKs-MacBook-Pro.local>
2019-10-04 15:21:53 -0700

    Made login, signup, and search components have uniform styling

--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -70,7 +70,7 @@ export default class App extends Component {
                 <IoIosLock className="icons" size={30} />
               </div>
               <div>
-                <p>By clicking Sign up or Continue with, I agree to Pavé's <u>Terms of Service</u>, <u>Payments Term of Service</u>, <u>Privacy Policy</u>, and <u>Nondiscrimination Policy</u></p>
+                <p className="signup-text">By clicking Sign up or Continue with, I agree to Pavé's <u>Terms of Service</u>, <u>Payments Term of Service</u>, <u>Privacy Policy</u>, and <u>Nondiscrimination Policy</u></p>
                 <button className="signing"><span className="sup-txt">Sign Up</span></button>
               </div>
             </form>
