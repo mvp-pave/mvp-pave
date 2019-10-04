8d8328ff2fc63e680365bed96ccbcabe9fa3e9a0
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2019-10-03 19:15:14 -0700

    Fixed carousel

--- client/src/components/SuggestedBottom.js
+++ client/src/components/SuggestedBottom.js
@@ -13,43 +13,45 @@ export default class SuggestedBottom extends Component {
       <div>
         <div className="sug-title">PAVÉ THE WAY</div>
         <div className="container-suggested-bottom">
-        <div className="arrow-container">
-          <div>
-            <IoIosArrowBack className="arrow-back" size={50}/>
+          <div className="arrow-container">
+            <div>
+              <IoIosArrowBack className="arrow-back" size={50} />
+            </div>
+          </div>
+          <div className="all-imgs">
+            <div className="img-container">
+              <img src="https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
+              <div className="title-container">
+                <div className="title">Food</div>
+              </div>
+            </div>
+            <div className="img-container">
+              <img src="https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
+              <div className="title-container">
+                <div className="title">Drinks</div>
+              </div>
+            </div>
+            <div className="img-container">
+              <img src="https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
+              <div className="title-container">
+                <div className="title">Experiences</div>
+              </div>
+            </div>
+            <div className="img-container">
+              <img src="https://images.unsplash.com/photo-1534224039826-c7a0eda0e6b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
+              <div className="title-container">
+                <div className="title">Connections</div>
+              </div>
+            </div>
+          </div>
+          <div className="arrow-container">
+            <div>
+              <IoIosArrowForward className="arrow-back" size={50} />
+            </div>
           </div>
         </div>
-        <div className="img-container">
-          <img src="https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
-          <div className="title-container">
-            <div className="title">Food</div>
-          </div>
-        </div>
-        <div className="img-container">
-          <img src="https://images.unsplash.com/photo-1551024709-8f23befc6f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
-          <div className="title-container">
-            <div className="title">Drinks</div>
-          </div>
-        </div>
-        <div className="img-container">
-          <img src="https://images.unsplash.com/photo-1528605248644-14dd04022da1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
-          <div className="title-container">
-            <div className="title">Experiences</div>
-          </div>
-        </div>
-        <div className="img-container">
-          <img src="https://images.unsplash.com/photo-1534224039826-c7a0eda0e6b3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"></img>
-          <div className="title-container">
-            <div className="title">Connections</div>
-          </div>
-        </div>
-        <div className="arrow-container">
-          <div>
-            <IoIosArrowForward className="arrow-back" size={50}/>
-          </div>
-        </div>
-      </div>
-    </div >
-  )
+      </div >
+    )
   }
 }
 
