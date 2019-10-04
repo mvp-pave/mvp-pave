e63da688955c09c55002f0b669edb3a8a6cde933
AK <ak@AKs-MacBook-Pro.local>
2019-10-04 09:33:18 -0700

    Fix search location styling

--- client/src/components/SearchLocation.js
+++ client/src/components/SearchLocation.js
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import { MdCancel } from "react-icons/md";
 
-let SearchLocation = ({ handleProfileIconClick }) => {
-  console.log('handleProfileIconClick', handleProfileIconClick)
-  return (
-    <div className="search-location-container">
-      <div className="icons-close-container">
-        <div className="close">
-          <MdCancel className="icons-close" size={30} onClick={handleProfileIconClick}/>
+export default class SearchLocation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render(){
+    let { handleProfileIconClick } = this.props;
+    return (
+      <div className="search-location-container">
+        <div className="icons-close-container">
+          <div className="close">
+            <MdCancel className="icons-close" size={30} onClick={handleProfileIconClick}/>
+          </div>
+        </div>
+        <div className="search-location-modal">
+        <form className="search-location-form">
+            <div className="search-location-form-container">
+              <input type="text" name="search-location" className="search-location" placeholder="Search Location"></input>
+              <MdCancel className="icons-search" size={30} />
+            </div>
+            <div>
+              <button className="searching-location"><span className="loc-txt">Use Current Location</span></button>
+            </div>
+        </form>
         </div>
       </div>
-      <div className="search-location-modal">
-      <form className="search-location-form">
-          <div className="search-location-form-container">
-            <input type="text" name="search-location" className="search-location" placeholder="Search Location"></input>
-            <MdCancel className="icons-search" size={30} />
-          </div>
-          <div>
-            <button className="searching-location"><span className="loc-txt">Use Current Location</span></button>
-          </div>
-      </form>
-      </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default SearchLocation;
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,6 @@ import List from './components/List.js';
 import CreateAccount from './components/CreateAccount.js';
 import Login from './components/Login.js'
 
-ReactDOM.render(<List />, document.getElementById('app'));
-// ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<List />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
