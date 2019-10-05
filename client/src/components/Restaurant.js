fe076cfed2367ee0fd09e9b095eba4a7c6387e5f
pauldev1128 <pauldev118@gmail.com>
2019-10-04 18:32:56 -0700

    worked on styling

--- client/src/components/Restaurant.js
+++ client/src/components/Restaurant.js
@@ -9,11 +9,9 @@ if (info) {
   return (
     <div className="restContainer">
       <div className="restName">{info.name}</div>
-      <div className="restAddress">{info.location.address1}</div>
-      <div className="restCity">{info.location.city}</div>
+      <div className="restAddress">{info.location.address1}<span className="restCity">{`, ${info.location.city}`}</span></div>
       <div className="restPhone">{info.phone}</div>
-      <div className="restRating">{info.rating}</div>
-      <div className="restReview">{info.review_count}</div>
+      <div className="restRating">{info.rating}<span className="restReview">{` (${info.review_count})`}</span></div>
       <img src={info.photos[2]} alt="" className="restPics"/>
     </div>
   )
