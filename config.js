9358844fb83c2301b7ab2b7c094f5ccedc160af8
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2019-10-05 10:16:01 -0700

    js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-let REACT_APP_YELP_API_KEY = function() {
+let REACT_APP_YELP_API_KEY = function () {
   return 'Bearer yN_rdAnzk7-agSRBHfbZlEoMRgj4wYUXJQ6gf_Qkf1r4aIVqTSFcYRcJ6xYJy1m5jKyLKOpP_KbNIw2eWHN9KFl_fpxDrztxQJtNLEbsTuCu3xACil__UHUu7iaWXXYx'
 }
 
