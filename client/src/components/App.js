fd45d430af905ee6b00c6d1678751c99cf429b7b
AK <ak@AKs-MacBook-Pro.local>
2019-10-05 12:12:26 -0700

    fix app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,7 +8,7 @@ import SuggestedBottom from './SuggestedBottom.js';
 import List from './List.js'
 import UpdateProfile from './UpdateProfile.js'
 
-import REACT_APP_YELP_API_KEY from '../../../config.js'
+import REACT_APP_YELP_API_KEY from '../../../yelpconfig.js'
 import axios from 'axios';
 
 import { IoMdSearch } from 'react-icons/io';
