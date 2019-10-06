1f2dd0947a5c1d95750b918406cd56cf5b9d5bb2
Emily Yang <emilyyang@emilys-air.lan>
2019-10-06 01:49:02 -0700

    added dropdown categories

--- client/src/components/TopBarMenu.js
+++ client/src/components/TopBarMenu.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MdLocationSearching, MdPeople } from "react-icons/md";
+
 
 let TopBarMenu = (props) => {
   if (props.currentUser) {
@@ -6,23 +8,26 @@ let TopBarMenu = (props) => {
       <div className='menu'>
         <div className='menu-list'>
           <p className="border-line" ></p>
-          <p onClick={() => location.reload()} >Sign Out</p>
-          <p>Settings</p>
-        </div>
+          <p className="dwnOptions" >Find Friends <MdLocationSearching color={'#bfbfbf'} className="dwnIcon" size={20} /></p>
+          <p className="dwnOptions">Groups <MdPeople color={'#bfbfbf'} size={20} className="dwnIcon" /> </p>
+          <p className="dwnOptions">Events</p>
+          <p className="dwnOptions">Settings</p>
+        <p className="dwnOptions"onClick={() => location.reload()} >Sign Out</p>
       </div>
+      </div >
     )
   } else {
-    return (
-      <div className='menu'>
-        <div className='menu-list'>
-          <p className="border-line" ></p>
-          <p onClick={props.clickHandler} className='login' >Sign In</p>
-          <p onClick={props.clickHandler} className='createAccount' >Sign Up</p>
-          <p className="setting">Settings</p>
-        </div>
+  return (
+    <div className='menu'>
+      <div className='menu-list'>
+        <p className="border-line" ></p>
+        <p className="dwnOptions" onClick={props.clickHandler} className='login' >Sign In</p>
+        <p className="dwnOptions" onClick={props.clickHandler} className='createAccount' >Sign Up</p>
+        <p className="dwnOptions" className="setting">Settings</p>
       </div>
-    )
-  }
+    </div>
+  )
+}
 }
 
 export default TopBarMenu;
